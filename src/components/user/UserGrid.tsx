e07b168514218df50a337c486337e5588f753b4e
humonnom <juepark42@gmail.com>
2025-11-26 16:02:31 +0900

    chore: Add Prettier configuration and ignore file; update ESLint config for Prettier integration

--- src/components/user/UserGrid.tsx
+++ src/components/user/UserGrid.tsx
@@ -3,7 +3,7 @@
 import * as React from 'react';
 import { Profile } from '@/types';
 import { UserCard } from './UserCard';
-import {UserCardSkeleton} from "@/components/skeletons";
+import { UserCardSkeleton } from '@/components/skeletons';
 
 interface UserGridProps {
   isLoading: boolean;
@@ -14,14 +14,9 @@ export const UserGrid = ({ isLoading, users }: UserGridProps) => {
   return (
     <div className="flex-1 overflow-y-auto">
       <div className="flex flex-row flex-wrap px-2.5 pb-5 pt-2.5 justify-between">
-          {isLoading && Array.from({ length: 6 }).map((_, index) => (
-              <UserCardSkeleton key={index} />
-          ))}
-          {!isLoading && users.map((user) => (
-          <UserCard key={user.id} user={user} />
-        ))}
+        {isLoading && Array.from({ length: 6 }).map((_, index) => <UserCardSkeleton key={index} />)}
+        {!isLoading && users.map((user) => <UserCard key={user.id} user={user} />)}
       </div>
     </div>
   );
 };
-
